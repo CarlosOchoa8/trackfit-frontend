67dbbba674383116e66afab3b880631a639a6220
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2025-06-01 12:00:27 -0600

    style: structure changed to fit content of body and header according dimensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
-// import './App.css'
+import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import MainDev from './components/MainDev'
-// import ExerciseForm from './components/Exercises/ExerciseForm'
-// import ExerciseItem from './components/Exercises/ExerciseItem'
 import ExerciseApp from './components/ExercisesApp'
-// import ExerciseApp from './components/Exercises/ExercisesApp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
-    <MainDev/>
-    <ExerciseApp/>
-    <Footer/>
-    </>
+      {/* <Header/> */}
+      {/* <MainDev/> */}
+      {/* <ExerciseApp/> */}
+      {/* <Footer/> */}
+      <Header />
+
+      <div className="app-body">
+        <ExerciseApp />
+      </div>
+
+      <Footer />
+      </>
   )
 }
 

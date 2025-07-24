d11c6665a8a0ef9568b896a407ba62661e41bad7
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2025-07-24 17:22:39 -0600

    feat: add func handle to wake up service

---
+++ src/helpers/back_service.js
@@ -0,0 +1,26 @@
+const renderService = async (url = import.meta.env.VITE_EXERCISE_BACKEND_API, timeout = 45000) => {
+    console.log("Waking up back service.")
+    console.log("URL =>", url)
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+    try {
+        const init = performance.now();
+        await fetch(url, {
+            method: "GET",
+            signal: controller.signal,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const end = performance.now();
+        const message = {"Ping": `${(end - init).toFixed(2)} ms`};
+        console.log(message)
+        return message
+    } catch(err) {
+        clearTimeout(timeoutId);
+        console.log("Service not available.", err)
+    }
+};
+
+export default renderService;
